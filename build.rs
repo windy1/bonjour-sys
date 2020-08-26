e7589e2835801db504824a60aa09d9b2ff9e38bb
Walker Crouse <Walker.Crouse@bjss.com>
2020-08-26 16:35:52 -0400

    Test
    Signed-off-by: Walker Crouse <Walker.Crouse@bjss.com>

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
 extern crate bindgen;
 
-use std::path::PathBuf;
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     bindgen::Builder::default()
         .header("wrapper.h")
+        .ctypes_prefix("::libc")
         .generate()
         .expect("failed to generate bindings")
         .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
