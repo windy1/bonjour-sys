2996c026bec6af800f00ee0df8ebc0a38dfd39cc
Walker Crouse <walkercrouse@hotmail.com>
2024-08-11 16:40:07 -0400

    allow clippy::all
    Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings2.rs"));
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
