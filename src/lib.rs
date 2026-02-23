c83c8e33fc7304cf17f7abc5108b2d93ac6db9d2
Walker Crouse <walker.crouse@acuitybrands.com>
2026-02-23 17:00:17 -0500

    upgrade rust edition
    Signed-off-by: Walker Crouse <walker.crouse@acuitybrands.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(unnecessary_transmutes)]
 #![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings2.rs"));
